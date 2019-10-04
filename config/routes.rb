de368828f3e8db7e434002a87a7971609d819743
tussi5969 <sakura.0304.0226@gmail.com>
2019-10-04 21:06:18 +0900

    クレジットカード登録ページのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  
+
 
   root 'items#index'
 
-  resources :users, only: [:index, :show]
+  # resources :users, only: [:index, :show]
   resources :items, only: [:index, :new]
 
   get 'users/logout'
+  get 'users/creditcard'
+  get 'users/addcreditcard'
   resources :users, only: [:index, :show, :edit]
 
 
