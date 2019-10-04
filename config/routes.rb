218634754322a24439053d2cf3503a6a39b2e5aa
kawadaren <renka19975@gmail.com>
2019-10-04 19:32:24 +0900

    マークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
 
-  
-
   root 'items#index'
 
   resources :users, only: [:index, :show]
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :show]
 
   get 'users/logout'
   resources :users, only: [:index, :show, :edit]
-  resources :items, only: [:index, :new, :show]
-
-
 
 end
