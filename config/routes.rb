057aed649a0077d47c82932816b3eda5066d85a6
kawadaren <renka19975@gmail.com>
2019-10-05 16:58:32 +0900

    画像ファイル一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
   resources :items, only: [:index, :new]
-
+  
   get 'users/logout'
   resources :users, only: [:index, :show, :edit]
 
