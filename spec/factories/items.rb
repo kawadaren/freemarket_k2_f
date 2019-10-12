58437e73d141802402f95e13a20c43c514ff1e18
kawadaren <renka19975@gmail.com>
2019-10-12 13:58:03 +0900

    テストコード関連

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,9 +1,9 @@
-require ‘rails_helper’
+# require ‘rails_helper’
 
-describe Item do
-describe ‘一覧表示機能’ do
-  it "商品の写真と名前と金額"
-    item = build(:item)
-    expect(user).to be_valid
-  end
-end
+# describe Item do
+# describe ‘一覧表示機能’ do
+#   it "商品の写真と名前と金額"
+#     item = build(:item)
+#     expect(user).to be_valid
+#   end
+# end
